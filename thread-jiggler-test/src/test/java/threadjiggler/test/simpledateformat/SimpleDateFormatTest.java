5a3962e2196d2f29c0f84463f515da71a46953e4
Alex Collins <alex.e.c@gmail.com>
2013-09-02 19:18:40 +0100

    forced UK locale

--- thread-jiggler-test/src/test/java/threadjiggler/test/simpledateformat/SimpleDateFormatTest.java
+++ thread-jiggler-test/src/test/java/threadjiggler/test/simpledateformat/SimpleDateFormatTest.java
@@ -1,5 +1,6 @@
 package threadjiggler.test.simpledateformat;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import threadjiggler.test.Jiggle;
@@ -8,8 +9,8 @@ import threadjiggler.test.Threads;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 import java.util.Random;
+import java.util.TimeZone;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 
@@ -25,19 +26,22 @@ public class SimpleDateFormatTest {
 	String[] formatted = new String[dates.length];
 
 	{
-		//final Date date = new Date(2012, 1, 7, 2, 4, 41);
-		//System.out.println("dateToFormatted.put(new Date(" + date.getTime()+"l), \"" + format.format(date) +"\");");
 		dates[0] = new Date(61338726000000l);
-		formatted[0] = "01/10/13 00:00";
+		formatted[0] = "30/09/13 11:00";
 		dates[1] = new Date(61322919781000l);
-		formatted[1] = "01/04/13 01:23";
+		formatted[1] = "01/04/13 12:23";
 		dates[2] = new Date(61286724281000l);
 		formatted[2] = "07/02/12 02:04";
 	}
 
-	SimpleDateFormat format = new SimpleDateFormat("dd/MM/YY hh:mm", Locale.ENGLISH);
+	SimpleDateFormat format = new SimpleDateFormat("dd/MM/YY hh:mm");
 	Random random = new Random();
 	int n = 1000;
+
+	@Before
+	public void setUp() throws Exception {
+		format.setTimeZone(TimeZone.getTimeZone("UTC"));
+	}
 
 	@Test
 	public void singleThreaded() throws Exception {
